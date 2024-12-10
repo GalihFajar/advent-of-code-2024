4a1350f357a8ff6d14ad4a87daa9838faf824fbe
GalihFajar <galihfajar00@gmail.com>
2024-12-10 15:39:41 +0700

    Solve day 10

---
+++ day_10.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  return 0;
+}
